4520cbeb3832d2585dd5eace657306b7330c0a28
AlfieriChou <alfierichou@gmail.com>
2019-08-14 14:58:43 +0800

    feat: lodash camelizeKeys

---
+++ src/lodash/camelizeKeys.ts
@@ -0,0 +1,20 @@
+import { camelCase } from 'lodash'
+
+interface CamelObject {
+  [s: string]: any
+}
+
+export const camelizeKeys = (obj: CamelObject): CamelObject => {
+  if (Array.isArray(obj)) {
+    return obj.map(v => camelizeKeys(v))
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce(
+      (result, key) => ({
+        ...result,
+        [camelCase(key)]: camelizeKeys(obj[key])
+      }),
+      {}
+    )
+  }
+  return obj
+}

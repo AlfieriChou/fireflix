7ad3be48af6ada8040e5d7d1132693dd87026652
AlfieriChou <alfierichou@gmail.com>
2019-08-14 10:11:18 +0800

    chore: init project

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  verbose: true
+}
---
+++ src/queue/queue.ts
@@ -0,0 +1,28 @@
+interface QueueOptions {
+  delayUntil?: number
+}
+
+export default class Queue<T> {
+  private list: T[] = []
+  private delayedItems: number = 0
+
+  public size(): number {
+    return this.list.length + this.delayedItems
+  }
+
+  public push(item: T, options: QueueOptions = {}): void {
+    if (options && options.delayUntil && options.delayUntil > Date.now()) {
+      this.delayedItems += 1
+      setTimeout(() => {
+        this.delayedItems -= 1
+        this.list.push(item)
+      }, options.delayUntil - Date.now())
+    } else {
+      this.list.push(item)
+    }
+  }
+
+  public shift(): T | undefined {
+    return this.list.shift()
+  }
+}

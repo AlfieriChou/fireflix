066019b10e8416b47e7447b0016d60449cddde81
AlfieriChou <alfierichou@gmail.com>
2019-08-17 22:40:13 +0800

    fix: incr key

--- src/rateLimit/redisStore.ts
+++ src/rateLimit/redisStore.ts
@@ -2,24 +2,22 @@ import * as IORedis from 'ioredis'
 
 export class RedisStore {
   private readonly redis: IORedis.Redis
-  private readonly key: string
   private readonly expire: number | undefined
-  constructor(options: IORedis.RedisOptions, key: string, expire?: number) {
+  constructor(options: IORedis.RedisOptions, expire?: number) {
     this.redis = new IORedis(options)
-    this.key = key
     this.expire = expire
   }
 
-  public async incr(): Promise<number> {
+  public async incr(key: string): Promise<number> {
     if (this.expire) {
       const result = await this.redis
         .multi()
-        .incr(this.key)
-        .expire(this.key, this.expire)
+        .incr(key)
+        .expire(key, this.expire)
         .exec()
       return result[0][1]
     }
-    const result = await this.redis.incr(this.key)
+    const result = await this.redis.incr(key)
     return result
   }
 }

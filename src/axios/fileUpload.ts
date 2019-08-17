493e0ba6276eedb1cc1d2e1aed235e7d52d57879
AlfieriChou <alfierichou@gmail.com>
2019-08-17 20:11:39 +0800

    feat: axios file upload

---
+++ src/axios/fileUpload.ts
@@ -0,0 +1,41 @@
+import * as FormData from 'form-data'
+import Axios from 'axios'
+import { promisify } from 'util'
+
+interface DataObj {
+  [s: string]: any
+}
+
+function buildForm (data: DataObj) {
+  const form = new FormData()
+  for (let item in data) {
+    form.append(item, data[item])
+  }
+  return form
+}
+
+async function getFormHeaders (form: FormData) {
+  const getLen = promisify(form.getLength).bind(form)
+  const len = await getLen()
+  return {
+    ...form.getHeaders(),
+    'Content-Length': len
+  }
+}
+
+export const fileUpload = async (url: string, data: DataObj, headers?: DataObj) => {
+  const formData = buildForm(data)
+  let formHeaders = await getFormHeaders(formData)
+  if (headers) {
+    formHeaders = {
+      ...headers,
+      ...formHeaders
+    }
+  } 
+  const res = await Axios.post(url, formData, {
+    headers: {
+      ...formHeaders
+    }
+  })
+  return res
+}

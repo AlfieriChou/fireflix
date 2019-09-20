8421f799a55069829d1ae32a07464c81125fac52
AlfieriChou <alfierichou@gmail.com>
2019-09-20 10:35:36 +0800

    chore: path parse

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const appRoot = require('app-root-path');
 const dir = require('dir_filenames');
 const _ = require('lodash');
+const path = require('path');
 
 function isClass(func) {
   return typeof func === 'function'
@@ -12,7 +13,7 @@ const files = dir(`${appRoot}/dist`);
 files.forEach(item => {
   /* eslint-disable import/no-dynamic-require */
   /* eslint-disable global-require */
-  const name = item.split('/').pop().replace(/\.\w+$/, '');
+  const name = path.parse(item).base.replace(/\.\w+$/, '');
   if (isClass(require(item).default)) {
     exports[_.upperFirst(name)] = require(item).default;
   } else {
